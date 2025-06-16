e94eb45ca9eed5caa5cfcd19dcbdd14e9b46e412
Pedro Guananja <pguananja@alegramed.com>
2025-06-15 21:22:44 -0300

    fix: nombre mal importado

--- src/pages/profesionales/routesProfesionales.ts
+++ src/pages/profesionales/routesProfesionales.ts
@@ -4,7 +4,7 @@ const profesionalesRoutes: RouteRecordRaw[] = [
     {
         path: '/listado-de-profesionales',
         name: 'listado-de-profesionales',
-        component: () => import('pages/profesionales/profesionalesView.vue')
+        component: () => import('pages/profesionales/ProfesionalesView.vue')
     }
 ]
 
